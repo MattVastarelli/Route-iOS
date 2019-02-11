1b1c89d9c1e2cf0b46b782553ce5fbcc412758b7
Vastarelli, Matthew P <mvast1@lab-bkh-240-m11.local>
2019-02-11 10:42:59 -0500

    simple models

---
+++ Route-iOS/Route-iOS/Models/Comment.swift
@@ -0,0 +1,13 @@
+//
+//  Comment.swift
+//  Route-iOS
+//
+//  Created by Vastarelli, Matthew P on 2/11/19.
+//  Copyright © 2019 Vastarelli, Matthew P. All rights reserved.
+//
+
+import Foundation
+
+class Comment: Post {
+
+}
---
+++ Route-iOS/Route-iOS/Models/User.swift
@@ -0,0 +1,70 @@
+//
+//  User.swift
+//  Route-iOS
+//
+//  Created by Vastarelli, Matthew P on 2/11/19.
+//  Copyright © 2019 Vastarelli, Matthew P. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    //Attributes
+    private var firstName: String
+    private var lastName: String
+    private var email: String
+    private var password: String
+    private var zipCode: Int
+    
+    //Initializer
+    init(fName: String, lName: String, email: String, pass: String, zip: Int) {
+        self.firstName = fName
+        self.lastName = lName
+        self.email = email
+        self.password = pass
+        self.zipCode = zip
+    }
+    //--------------------------------------------------------------------------------
+    // Account Getters
+    func getFirstName() -> String {
+        return self.firstName
+    }
+    
+    func getLastName() -> String {
+        return self.lastName
+    }
+    
+    func getEmail() -> String {
+        return self.email
+    }
+    
+    func getPass() -> String {
+        return self.password
+    }
+    
+    func getZip() -> Int {
+        return self.zipCode
+    }
+    
+    // Account Setters
+    func setFirstName(name: String){
+        self.firstName = name
+    }
+    
+    func setLastName(name: String){
+        self.lastName = name
+    }
+    
+    func setPass(pass: String){
+        self.password = pass
+    }
+    
+    func setEmail(email: String){
+        self.email = email
+    }
+    
+    func setZip(zip: Int){
+        self.zipCode = zip
+    }
+    //----------------------------------------------------------------------------------
+}
