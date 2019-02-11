e5c2eea27f03e404b6ffebab4a3d4afb91b4d0d4
Vastarelli, Matthew P <mvast1@lab-bkh-240-m11.local>
2019-02-11 10:57:23 -0500

    post class

--- Route-iOS/Route-iOS/Models/Post.swift
+++ Route-iOS/Route-iOS/Models/Post.swift
@@ -9,5 +9,51 @@
 import Foundation
 
 class Post {
+    private var author: User
+    private var title: String
+    private var postBody: String
+    private var routeOfPost: Route
     
+    //Initializer
+    init(author: User, title: String, body: String, route: Route) {
+        self.author = author
+        self.title = title
+        self.postBody = body
+        self.routeOfPost = route
+    }
+    //----------------------------------------------------------------
+    // Post Getters
+    func getAuthor() -> User {
+        return self.author
+    }
+    
+    func getTitle() -> String {
+        return self.title
+    }
+    
+    func getPostBody() -> String {
+        return self.postBody
+    }
+    
+    func getRouteOfPost() -> Route {
+        return self.routeOfPost
+    }
+    
+    // Post Setters
+    func setTitle(title: String) {
+        self.title = title
+    }
+    
+    func setPostBody(body: String) {
+        self.postBody = body
+    }
+    
+    func setAuthor(author: User) {
+        self.author = author
+    }
+    
+    func setRouteOfPost(route: Route) {
+        self.routeOfPost = route
+    }
+    //-----------------------------------------------------------------------
 }
