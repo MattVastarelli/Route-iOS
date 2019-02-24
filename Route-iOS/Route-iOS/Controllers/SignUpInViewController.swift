84100d563c0d9f51020744bf03413ce136ae86a5
matthew vastarelli <matthew@matthews-mini.frontier.com>
2019-02-24 17:38:37 -0500

    basic signup done

---
+++ Route-iOS/Route-iOS/Controllers/SignUpInViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SignUpInViewController.swift
+//  Route-iOS
+//
+//  Created by matthew vastarelli on 2/24/19.
+//  Copyright © 2019 Vastarelli, Matthew P. All rights reserved.
+//
+
+import UIKit
+import FirebaseUI
+import Firebase
+import FirebaseAuth
+
+class SignUpInViewController: UIViewController {
+    
+    // sign up text feilds
+    @IBOutlet weak var emailFeild: UITextField!
+    @IBOutlet weak var passwordFeild: UITextField!
+    
+    //output label
+    @IBOutlet weak var outputLabel: UILabel!
+    @IBOutlet weak var failbox: UITextView!
+    
+    // grab the email and password from the feilds
+    @IBAction func signUpButton(_ sender: UIButton) {
+        let email = emailFeild.text
+        let password = passwordFeild.text
+        
+        Auth.auth().createUser(withEmail: email!, password: password!, completion: {(user, error) in
+            if error == nil {
+                self.outputLabel.text = "You are successfully registered"
+            }else{
+                self.outputLabel.text = "Registration Failed.. Please Try Again"
+                self.failbox.text = error!.localizedDescription
+            }
+            
+        })
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
