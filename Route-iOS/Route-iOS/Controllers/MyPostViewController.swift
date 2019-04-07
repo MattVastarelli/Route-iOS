50c925955401bdb4f88949ca6ee3da70fa605829
Matt Vastarelli <mvast1@unh.newhaven.edu>
2019-04-07 15:55:57 -0400

    fill route display

--- Route-iOS/Route-iOS/Controllers/MyPostViewController.swift
+++ Route-iOS/Route-iOS/Controllers/MyPostViewController.swift
@@ -11,12 +11,35 @@ import Firebase
 
 class MyPostViewController: UIViewController {
     
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var attributeLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var terainLabel: UILabel!
+    
     var myPost: String = ""
+    // container to hold the route passed from the post vc
+    //as the ID produced by firebase does not exist till after
+    // the segue
     var container = Array<Any>()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("container", container)
+        self.setLabels()
+    }
+    
+    func setLabels() {
+        let route = container[0] as! RoutePost
+        
+        self.descriptionLabel.text = route.getPostBody()
+        self.titleLabel.text = route.getTitle()
+        self.speedLabel.text = route.getRoute().getTrafficSpeed()
+        self.locationLabel.text = route.getRoute().getLocationType()
+        self.typeLabel.text = route.getRoute().getActivityType()
+        self.attributeLabel.text = route.getRoute().getAttributes().joined(separator: ", ")
+        self.terainLabel.text = route.getRoute().getTerain()
     }
     
 
